14bfef65cd806868cf58da1c2ec3392f535818d1
Santiago Fernandez <santiagoagustinfernandez@gmail.com>
2023-11-08 18:36:06 -0300

    New Questions

--- sql/app_insert.py
+++ sql/app_insert.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect('sql/QuestionsAnswersDB')
 cursor = conn.cursor()
 
 cursor.execute("INSERT INTO Questions (QuestionText) VALUES (?)",
-               ("What type of PII is controlled based on laws and carries legal penalties for noncompliance with requirements?",))
+               ("Which of the following is NOT a factor that is part of a firewall configuration?",))
 
 # Get the ID of the newly inserted question
 cursor.execute("SELECT last_insert_rowid() AS LastID")
@@ -15,10 +15,10 @@ last_inserted_id = cursor.fetchone()[0]
 
 # Insert the answers for that question into the "Answers" table
 answers = [
-    (last_inserted_id, 'Regulated', 1),
-    (last_inserted_id, 'Contractual', 0),
-    (last_inserted_id, 'Specific', 0),
-    (last_inserted_id, 'Jurisdictional', 0)
+    (last_inserted_id, 'Encryption', 1),
+    (last_inserted_id, 'Port', 0),
+    (last_inserted_id, 'Protocol', 0),
+    (last_inserted_id, 'Source IP', 0)
 ]
 
 cursor.executemany("INSERT INTO Answers (QuestionID, AnswerText, IsCorrect) VALUES (?, ?, ?)", answers)
